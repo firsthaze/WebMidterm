07036130a11ea5317ae60100786d1141e0460fc8
shanshow <showdiango@gmail.com>
2016-04-15 18:29:27 +0800

    排版更改以及增加按鈕特效

--- js/shop1.js
+++ js/shop1.js
@@ -7,6 +7,7 @@ var main = function() {
     number4: 0,
     number5: 0
   });*/
+  $(".estimate").hide();
   myDataRef.child("rate").on('value', function (snapshot) {
 var message = snapshot.val();
 var counting1 = message.number1;
@@ -22,27 +23,31 @@ $('#count5').html(counting5);
 $('#rate1').click(function(){
   counting1 = counting1 + 1;
   myDataRef.child("rate").update({number1: counting1});
-  $('#count1').html(counting1)
+  $('#count1').html(counting1);
+  close();
 });
 $('#rate2').click(function(){
   counting2 = counting2 + 1;
   myDataRef.child("rate").update({number2: counting2});
-  $('#count2').html(counting2)
+  $('#count2').html(counting2);
+  close();
 });
 $('#rate3').click(function(){
   counting3 = counting3 + 1;
   myDataRef.child("rate").update({number3: counting3});
-  $('#count3').html(counting3)
+  $('#count3').html(counting3);
+  close();
 });
 $('#rate4').click(function(){
   counting4 = counting4 + 1;
   myDataRef.child("rate").update({number4: counting4});
-  $('#count4').html(counting4)
+  $('#count4').html(counting4);
+  close();
 });
 $('#rate5').click(function(){
   counting5 = counting5 + 1;
   myDataRef.child("rate").update({number5: counting5});
-  $('#count5').html(counting5)
+  $('#count5').html(counting5);
   close();
 });
 $('#score').html((counting5*5+
@@ -53,6 +58,7 @@ $('#score').html((counting5*5+
 });
 var close = function()
 {
+$(".estimate").slideDown(300).show();
 $(".button").slideUp(300).addClass(displaynone);
 }
 
