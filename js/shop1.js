8167de2354aa5a177c43b82a2c41494f5cb2601a
shanshow <showdiango@gmail.com>
2016-04-19 15:26:47 +0800

    加圖

--- js/shop1.js
+++ js/shop1.js
@@ -1,6 +1,6 @@
 var main = function() {
   var myDataRef = new Firebase('https://ratingstar.firebaseio.com/');
-  /*myDataRef.child("rate").set({
+  /*myDataRef.child("rate1").set({
     number1: 0,
     number2: 0,
     number3: 0,
@@ -8,7 +8,7 @@ var main = function() {
     number5: 0
   });*/
   $(".estimate").hide();
-  myDataRef.child("rate").on('value', function (snapshot) {
+  myDataRef.child("rate1").on('value', function (snapshot) {
 var message = snapshot.val();
 var counting1 = message.number1;
 var counting2 = message.number2;
@@ -22,31 +22,31 @@ $('#count4').html(counting4);
 $('#count5').html(counting5);
 $('#rate1').click(function(){
   counting1 = counting1 + 1;
-  myDataRef.child("rate").update({number1: counting1});
+  myDataRef.child("rate1").update({number1: counting1});
   $('#count1').html(counting1);
   close();
 });
 $('#rate2').click(function(){
   counting2 = counting2 + 1;
-  myDataRef.child("rate").update({number2: counting2});
+  myDataRef.child("rate1").update({number2: counting2});
   $('#count2').html(counting2);
   close();
 });
 $('#rate3').click(function(){
   counting3 = counting3 + 1;
-  myDataRef.child("rate").update({number3: counting3});
+  myDataRef.child("rate1").update({number3: counting3});
   $('#count3').html(counting3);
   close();
 });
 $('#rate4').click(function(){
   counting4 = counting4 + 1;
-  myDataRef.child("rate").update({number4: counting4});
+  myDataRef.child("rate1").update({number4: counting4});
   $('#count4').html(counting4);
   close();
 });
 $('#rate5').click(function(){
   counting5 = counting5 + 1;
-  myDataRef.child("rate").update({number5: counting5});
+  myDataRef.child("rate1").update({number5: counting5});
   $('#count5').html(counting5);
   close();
 });
